609bdd82e01fc7933e831612e9da4763839a03d1
Mikel Muennekhoff <mikel.muennekhoff@via-ev.de>
2020-04-09 21:38:53 +0200

    Refactor frontend js
    Condense redundant code. Therefore, track colors as object rather than a bool for each color.

--- js/cards.js
+++ js/cards.js
@@ -1,3 +1,12 @@
+var ownColors = {
+  yellow: false,
+  blue: false,
+  green: false,
+  red: false,
+  white: false,
+  all: false,
+};
+
 $(document).ready(() => {
   let wsUrl =
     (location.protocol == "https:" ? "wss://" : "ws://") +
@@ -65,103 +74,34 @@ $(document).ready(() => {
   }
 
   function countCards() {
-    ["yellow", "blue", "green", "red", "white", "all"].forEach((color) => {
+    Object.keys(ownColors).forEach((color) => {
       $(`#count-${color}`).text($(`#cards .card-${color}`).length);
     });
   }
 
-  var yellow = false;
-  var blue = false;
-  var green = false;
-  var red = false;
-  var white = false;
-  var all = false;
-
   function resetOwnCards() {
-    ["yellow", "blue", "green", "red", "white", "all"].forEach((color) => {
+    Object.keys(ownColors).forEach((color) => {
       $(`#${color}Btn`).addClass("btn-color-unselected");
       $(`#${color}Btn`).removeClass("btn-color-selected");
+      ownColors[color] = false;
     });
-    yellow = false;
-    blue = false;
-    green = false;
-    red = false;
-    white = false;
-    all = false;
   }
 
-  $("#yellowBtn").on("click", function (event) {
-    event.preventDefault();
-    socket.send(
-      JSON.stringify({
-        type: yellow ? "lower" : "raise",
-        card: "yellow",
-      })
-    );
-    yellow = !yellow;
-    $(this).toggleClass("btn-color-unselected");
-    $(this).toggleClass("btn-color-selected");
-  });
-  $("#blueBtn").on("click", function (event) {
-    event.preventDefault();
-    socket.send(
-      JSON.stringify({
-        type: blue ? "lower" : "raise",
-        card: "blue",
-      })
-    );
-    blue = !blue;
-    $(this).toggleClass("btn-color-unselected");
-    $(this).toggleClass("btn-color-selected");
-  });
-  $("#greenBtn").on("click", function (event) {
-    event.preventDefault();
-    socket.send(
-      JSON.stringify({
-        type: green ? "lower" : "raise",
-        card: "green",
-      })
-    );
-    green = !green;
-    $(this).toggleClass("btn-color-unselected");
-    $(this).toggleClass("btn-color-selected");
-  });
-  $("#redBtn").on("click", function (event) {
-    event.preventDefault();
-    socket.send(
-      JSON.stringify({
-        type: red ? "lower" : "raise",
-        card: "red",
-      })
-    );
-    red = !red;
-    $(this).toggleClass("btn-color-unselected");
-    $(this).toggleClass("btn-color-selected");
-  });
-  $("#whiteBtn").on("click", function (event) {
-    event.preventDefault();
-    socket.send(
-      JSON.stringify({
-        type: white ? "lower" : "raise",
-        card: "white",
-      })
-    );
-    white = !white;
-    $(this).toggleClass("btn-color-unselected");
-    $(this).toggleClass("btn-color-selected");
-  });
-  $("#allBtn").on("click", function (event) {
-    event.preventDefault();
-    socket.send(
-      JSON.stringify({
-        type: all ? "lower" : "raise",
-        card: "all",
-      })
-    );
-    all = !all;
-    $(this).toggleClass("btn-color-unselected");
-    $(this).toggleClass("btn-color-selected");
-  });
+  Object.keys(ownColors).forEach((color) => {
+    $(`#${color}Btn`).on("click", function (event) {
+      event.preventDefault();
+      socket.send(
+        JSON.stringify({
+          type: ownColors[color] ? "lower" : "raise",
+          card: `${color}`,
+        })
+      );
+      ownColors[color] = !ownColors[color];
+      $(this).toggleClass("btn-color-unselected");
+      $(this).toggleClass("btn-color-selected");
+    });
+  })
+  
   $("#resetBtn").on("click", function (event) {
     event.preventDefault();
     socket.send(
